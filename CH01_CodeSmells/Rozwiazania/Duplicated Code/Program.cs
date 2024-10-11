b703a8eb5b8e2dc77491582d4355dcd5b41371ce
Yamca4231 <56595496+Yamca4231@users.noreply.github.com>
2024-10-11 20:05:41 +0200

    Duplicated_Code

---
+++ CH01_CodeSmells/Rozwiazania/Duplicated Code/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TASK02Duplicated_Code
+{
+    class Duplicated_Code
+    {
+            public void LogError(string message, DateTime timestamp)
+        {
+            string formattedMessage = FormatLogMessage("ERROR",message, timestamp);
+            Console.WriteLine(formattedMessage);
+        }
+
+        public void LogWarning(string message, DateTime timestamp)
+        {
+            
+            string formattedMessage = FormatLogMessage("WARNING", message, timestamp);
+            Console.WriteLine(formattedMessage);
+        }
+        private string FormatLogMessage(string logLevel, string message, DateTime timestamp)
+        {
+            string formattedTimestamp = timestamp.ToString("yyyy-MM-dd HH:mm:ss");
+            return $"{logLevel}: [{formattedTimestamp}] {message}";
+        }
+    }
+}
