d9ca60a238e1f1c61bf7c687371991d791e449ae
Yamca4231 <56595496+Yamca4231@users.noreply.github.com>
2024-10-11 19:31:02 +0200

    LongParameterList

---
+++ CH01_CodeSmells/Rozwiazania/Long Parameter List/LongParameterList/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task01LongParameterList
+{
+    class LongParameterList
+    {
+        public void RegisterProduct(Product product)
+        {
+            // Rejestracja produktu
+            Console.WriteLine($"Product: {product.productName}, Category: {product.category}, Price: {product.price:C}, Stock: {product.stock}, Supplier: {product.supplierName}, Contact: {product.supplierContact}");
+        }
+    }
+    class Product
+    {
+        internal string productName { get; set; }
+        internal string category { get; set; }
+        internal decimal price { get; set; }
+        internal int stock { get; set; }
+        internal string supplierName { get; set; }
+        internal string supplierContact { get; set; }
+
+    }
+}
