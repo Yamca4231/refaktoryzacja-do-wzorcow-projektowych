cf92269459561aa03daa3152a0a407780c947a7b
Yamca4231 <56595496+Yamca4231@users.noreply.github.com>
2024-12-17 00:05:57 +0100

    CW 2 (11.10) - MessageChains

---
+++ CH01_CodeSmells/Rozwiazania/MessageChains/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace MessageChains
+{
+    internal class MessageChains
+    {
+        public class Car
+        {
+            public string GetCylinderSize()
+            {
+                Engine engine = new Engine();
+                return engine.GetCylinder().GetSize();
+            }
+        }
+
+        public class Engine
+        {
+            public Cylinder GetCylinder()
+            {
+                return new Cylinder();
+            }
+        }
+
+        public class Cylinder
+        {
+            public string GetSize()
+            {
+                return "2.0L";
+            }
+        }
+
+        public class Program
+        {
+            public static void Main(string[] args)
+            {
+                // Przykład wywołania
+                Car car = new Car();
+                string cylinderSize = car.GetCylinderSize();
+
+                Console.WriteLine($"Cylinder size: {cylinderSize}");
+            }
+        }
+    }
+   
+
+}
+
+//Masz kod, który wywołuje łańcuch metod z Library.GetBook().GetAuthor().GetName(). Twoim zadaniem jest wprowadzenie nowej metody GetAuthorName w klasie Library, aby zredukować łańcuch wiadomości.
+//Kroki do wykonania:
+// 
+// 1. Wprowadź metodę GetCylinderSize w klasie Car.
+// 1. Przenieś wywołanie zagnieżdżone do nowej metody w klasie Car.
+// 1. Zaktualizuj wywołania metod, aby używały nowej metody.
