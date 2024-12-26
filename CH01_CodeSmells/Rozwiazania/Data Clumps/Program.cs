99fdf0dc5a3cac6c84110952ba9d7236005bc347
Yamca4231 <56595496+Yamca4231@users.noreply.github.com>
2024-12-26 16:27:24 +0100

    Ćw 3 (25.10) Data Clumps

---
+++ CH01_CodeSmells/Rozwiazania/Data Clumps/Program.cs
@@ -0,0 +1,21 @@
+﻿public class EventDetails()
+{
+    public string EventName { get; set; }
+    public DateTime EventDate { get; set; }
+    public string Location { get; set; }
+
+    public void RegisterEvent(EventDetails detail)
+    {
+        Console.WriteLine($"Event: {detail.EventName}, Date: {detail.EventDate}, Location: {detail.Location}");
+    }
+}
+
+// Przykład użycia
+class Program
+{
+    static void Main(string[] args)
+    {
+        var eventDetails = new EventDetails { EventName = "Konferencja", EventDate = new DateTime(2024, 12, 30), Location = "Warszawa" };
+        eventDetails.RegisterEvent(eventDetails);
+    }
+}
