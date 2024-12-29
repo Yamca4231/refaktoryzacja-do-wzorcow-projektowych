5cd3c02d256b3ceaad77cea1c067f0fe95dd6e40
Yamca4231 <56595496+Yamca4231@users.noreply.github.com>
2024-12-29 15:46:16 +0100

    Ćw 5 (29.11) Composite

---
+++ CH06_NetDesignPatterns/Zadania/Composite/MenuStructure.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Interfejs wspólny dla pozycji menu
+public interface IMenuComponent
+{
+    void Display(string indent);
+}
+
+// Klasa liścia: Danie
+public class MenuItem : IMenuComponent
+{
+    public string Name { get; }
+    public decimal Price { get; }
+
+    public MenuItem(string name, decimal price)
+    {
+        Name = name;
+        Price = price;
+    }
+
+    public void Display(string indent)
+    {
+        Console.WriteLine($"{indent}{Name} - {Price:F2} PLN");
+    }
+}
+
+// Klasa kompozytu: Kategoria menu
+public class MenuCategory : IMenuComponent
+{
+    public string Name { get; }
+    private readonly List<IMenuComponent> _components = new List<IMenuComponent>();
+
+    public MenuCategory(string name)
+    {
+        Name = name;
+    }
+
+    public void Add(IMenuComponent component)
+    {
+        _components.Add(component);
+    }
+
+    public void Remove(IMenuComponent component)
+    {
+        _components.Remove(component);
+    }
+
+    public void Display(string indent)
+    {
+        Console.WriteLine($"{indent}{Name}:");
+        foreach (var component in _components)
+        {
+            component.Display(indent + "  ");
+        }
+    }
+}
---
+++ CH06_NetDesignPatterns/Zadania/Composite/Program.cs
@@ -0,0 +1,31 @@
+﻿// Program główny
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Tworzenie liści (pozycji menu)
+        var pierogi = new MenuItem("Pierogi ruskie", 25.99m);
+        var schabowy = new MenuItem("Kotlet schabowy", 34.50m);
+        var zurek = new MenuItem("Żurek", 18.00m);
+        var kompot = new MenuItem("Kompot", 7.00m);
+
+        // Tworzenie kompozytów (kategorii menu)
+        var daniaGlowne = new MenuCategory("Dania Główne");
+        daniaGlowne.Add(pierogi);
+        daniaGlowne.Add(schabowy);
+
+        var zupy = new MenuCategory("Zupy");
+        zupy.Add(zurek);
+
+        var napoje = new MenuCategory("Napoje");
+        napoje.Add(kompot);
+
+        var menu = new MenuCategory("Menu Restauracji Polskiej");
+        menu.Add(zupy);
+        menu.Add(daniaGlowne);
+        menu.Add(napoje);
+
+        // Wyświetlenie pełnego menu
+        menu.Display(string.Empty);
+    }
+}
