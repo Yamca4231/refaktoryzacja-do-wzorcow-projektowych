8a3a9e8daaf3c1f841ffc23566d838fdfdf67ce0
Yamca4231 <56595496+Yamca4231@users.noreply.github.com>
2024-12-29 16:00:04 +0100

    Ćw 5 (29.11) Flyweight

---
+++ CH06_NetDesignPatterns/Zadania/Flyweight/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var factory = new TextureFactory();
+
+        // Tworzenie obiektów z teksturami
+        var player = new GameObject("Player", factory.GetTexture("grass.png"));
+        var enemy = new GameObject("Enemy", factory.GetTexture("stone.png"));
+        var tree = new GameObject("Tree", factory.GetTexture("grass.png"));
+
+        // Renderowanie
+        player.Render();
+        enemy.Render();
+        tree.Render();
+    }
+}
---
+++ CH06_NetDesignPatterns/Zadania/Flyweight/TexturesFlyweight.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Tekstura współdzielona
+public class Texture
+{
+    public string FileName { get; }
+
+    public Texture(string fileName)
+    {
+        FileName = fileName;
+        Console.WriteLine($"Loading texture: {fileName}");
+    }
+
+    public void Display(string context)
+    {
+        Console.WriteLine($"Displaying '{FileName}' for {context}");
+    }
+}
+
+// Fabryka tekstur
+public class TextureFactory
+{
+    private readonly Dictionary<string, Texture> _textures = new();
+
+    public Texture GetTexture(string fileName)
+    {
+        if (!_textures.ContainsKey(fileName))
+        {
+            _textures[fileName] = new Texture(fileName);
+        }
+        return _textures[fileName];
+    }
+}
+
+// Obiekt gry
+public class GameObject
+{
+    public string Name { get; }
+    private readonly Texture _texture;
+
+    public GameObject(string name, Texture texture)
+    {
+        Name = name;
+        _texture = texture;
+    }
+
+    public void Render()
+    {
+        Console.WriteLine($"Rendering {Name}");
+        _texture.Display(Name);
+    }
+}
